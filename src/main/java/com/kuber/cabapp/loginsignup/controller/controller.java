1eab8375a1c0f0aa0e6c520eba76fb3e53ea153c
Debasmita Bhattacharyya <debasmitabhattacharyya@Debasmitas-MacBook-Air.local>
2023-06-29 14:01:29 +0530

    initiated repo

---
+++ src/main/java/com/kuber/cabapp/loginsignup/controller/controller.java
@@ -0,0 +1,17 @@
+package com.kuber.cabapp.loginsignup.controller;
+
+import com.kuber.cabapp.loginsignup.user.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @PostMapping("/signup")
+    public void sign_Up(@RequestBody User user)
+    {
+
+    }
+}
---
+++ src/main/java/com/kuber/cabapp/loginsignup/user/User.java
@@ -0,0 +1,58 @@
+package com.kuber.cabapp.loginsignup.user;
+
+public class User {
+    private String username;
+    private String first_Name;
+    private String last_Name;
+    private String contactNo;
+    private String emailID;
+    private String city;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_Name() {
+        return first_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        this.first_Name = first_Name;
+    }
+
+    public String getLast_Name() {
+        return last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        this.last_Name = last_Name;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
